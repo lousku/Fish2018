a3946824e86bc12dfcf1e625b18cfa2e765deedf
Miika Suomalainen <miika.suomalainen@eatech.fi>
2018-02-20 22:13:52 +0200

    jiihaaa

--- harj1/FisTestiHTML.js
+++ harj1/FisTestiHTML.js
@@ -1,21 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var request = require('request');
 
-var seriveWakeURI = 'http://escop.rd.tut.fi:3000/RTU/SimROB1/services/LoadPaper'
-var destURL = "http://escop.rd.tut.fi:3000/fmw"
+var seriveWakeURI = 'http://192.168.100.113/rest/events/time/notifs'
+var destURL = "http://192.168.0.1:3000"
 
-request.post(
-    seriveWakeURI, { json: {"destUrl": destURL} },
-    function (error, response, body) {
+request.post(seriveWakeURI, { json: {"destUrl": destURL} },
+function (error, response, body) {
+        console.log('body');
         console.log(body);
-        console.log('paperi ladattu');
+        console.log('response');
+      //  console.log(response);
+
     }
 );
 
+app.post('/', function(req,res){
+  console.log(res);
+  console.log(req);
+});
 
+
+
+/*
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
+  console.log("nettisivu ladattiin");
 });
 
 io.on('connection', function(socket){
@@ -25,7 +36,7 @@ io.on('connection', function(socket){
     io.emit('viestin valitys',msg);
   });
 });
-
+*/
 http.listen(3000, function(){
   console.log('listening on 3000');
 });
