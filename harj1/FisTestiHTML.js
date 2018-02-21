73fa497c1aa5ad09a27e25648d1d4f118e5bfca8
Miika Suomalainen <miika.suomalainen@eatech.fi>
2018-02-21 21:07:53 +0200

    paskaa

--- harj1/FisTestiHTML.js
+++ harj1/FisTestiHTML.js
@@ -3,40 +3,53 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('request');
 
-var seriveWakeURI = 'http://192.168.100.113/rest/events/time/notifs'
+var seriveWakeURI = 'http://192.168.100.113/rest/services/startEvents'
+var serviceUrl = 'http://192.168.100.113/rest/events'
 var destURL = "http://192.168.0.1:3000"
 
-request.post(seriveWakeURI, { json: {"destUrl": destURL} },
-function (error, response, body) {
-        console.log('body');
-        console.log(body);
-        console.log('response');
-      //  console.log(response);
 
-    }
-);
+//
 
-app.post('/', function(req,res){
-  console.log(res);
-  console.log(req);
+
+app.post('', function(req,res){
+  console.log('post request received: ');
+  io.emit('messageToUi',"post request received: " + req);
+  res.write('amazing!');
+  res.end();
 });
 
+app.on('data',function(){
+  console.log('received!!!');
+});
 
-
-/*
+//
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
-  console.log("nettisivu ladattiin");
+  console.log("HTML User Interface opened");
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('viestin valitys',msg);
+
+  socket.on('event request', function(){
+    request.post(seriveWakeURI, { json: {"destUrl": destURL} },
+    function (error, response, body) {
+            console.log('event request done, returned body: ' + JSON.stringify(body));
+            io.emit('messageToUi',"Request done, received body: " + JSON.stringify(body));
+    });
   });
+
+  socket.on('get services', function(){
+    request.get(serviceUrl, { json: {"destUrl": destURL} },
+    function (error, response, body) {
+      console.log('available services: '+ JSON.stringify(body));
+      io.emit('messageToUi','available services: '+ JSON.stringify(body))
+    });
+  });
+
 });
-*/
+
+
 http.listen(3000, function(){
   console.log('listening on 3000');
 });
---
+++ harj1/postTest.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var http = require('http');
+
+http.createServer((request, response) => {
+  const { headers, method, url } = request;
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    console.log(body);
+    console.log(method);
+    console.log(url);
+  });
+}).listen(3000);
