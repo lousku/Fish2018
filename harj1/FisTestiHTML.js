bbc4c95bc5dd01c56f279daec2e393590283e450
Miika Suomalainen <miika.suomalainen@eatech.fi>
2018-02-22 20:33:44 +0200

    jkk

--- harj1/FisTestiHTML.js
+++ harj1/FisTestiHTML.js
@@ -29,17 +29,11 @@ app.get('/', function(req,res){
   console.log("HTML User Interface opened");
 });
 
+//Kun nettiselaimella mennään localhost:3000
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.on('event request', function(){
-    request.post(seriveWakeURI,  '{"destUrl": destURL}' ,
-    function (error, response, body) {
-            console.log('event request done, returned body: ' + JSON.stringify(body));
-            io.emit('messageToUi',"Request done, received body: " + JSON.stringify(body));
-    });
-  });
-
+  //Kun käyttiksen kautta tulee "get services"-käsky
   socket.on('get services', function(){
     request.get(serviceUrl, { json: {"destUrl": destURL} },
     function (error, response, body) {
@@ -48,20 +42,17 @@ io.on('connection', function(socket){
     });
   });
 
-
+//Kun käyttiksen kautta tulee wake event - käsky
   socket.on('wake event', function(uriValue, sentBody){
     changeLights(sentBody);
-  /*  request.post(IPadressS1000+uriValue.toString()  , {body: {"state0":totuus,"state1":totuus,"state2":totuus,"state3":true,"state4":true,"state5":true,"state6":true,"state7":true}, json: true },
-    function (error, response, body) {
-      console.log('event waked at:' + IPadressS1000 +uriValue + ' body received: '+ JSON.stringify(body));
-      io.emit('messageToUi','event waked at:' + IPadressS1000 +uriValue +   ' body received: '+ JSON.stringify(body));
-    });*/
+
   });
 
+  //8 bits string in example 11110000
   function changeLights(trueStates) {
 
     var States = [false, false, false,false,false,false,false,false];
-
+    //käydään läpi truestates stringin merkit, jos 1
     for (i = 0; i < trueStates.length; i++) {
         if(trueStates.charAt(i) == 1){
           States[i] = true;
@@ -70,14 +61,14 @@ io.on('connection', function(socket){
         else{
           States[i] = false;
         }
-}
+    }
 
     request.post(IPadressS1000+"/rest/services/changeOutput"  , {body: {"state0":States[0],"state1":States[1],"state2":States[2],"state3":States[3],"state4":States[4],"state5":States[5],"state6":States[6],"state7":States[7]}, json: true },
     function (error, response, body) {
       console.log('event waked at:' + IPadressS1000 +"/rest/services/changeOutput" + ' body received: '+ JSON.stringify(body));
       io.emit('messageToUi','event waked at:' + IPadressS1000 +"/rest/services/changeOutput" +' body received: '+ JSON.stringify(body));
     });
-}
+  }
 });
 
 http.listen(3000, function(){
