8cb5d7aaec9c34ee3c511dae2dbf5696074b9ab2
Miika Suomalainen <miika.suomalainen@eatech.fi>
2018-02-26 21:16:04 +0200

    toimiva ilman SOAPPIA

--- harj1/FisTestiHTML.js
+++ harj1/FisTestiHTML.js
@@ -3,26 +3,42 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('request');
 
+const bodyParse=require('body-parser');
+
+app.use(bodyParse.json());
 
 
 var seriveWakeURI = 'http://192.168.100.113/rest/services/startEvents'
-var eventWakeUp = 'http://192.168.100.113/rest/events/timeWS'
 var serviceUrl = 'http://192.168.100.113/rest/events'
-var destURL = "http://192.168.0.1:3000" //my own IP
+var SubscribeUrl = 'http://192.168.100.113/rest/events/time/notifs/'
+var outputUri = 'http://192.168.100.113/rest/services/changeOutput'
+var destURL = "http://192.168.0.1:3456" //my own IP
 var IPadressS1000 = "http://192.168.100.113"   //S1000 IP
+
 
 //
 
 
 app.post('', function(req,res){
-  console.log('post request received: ');
-  io.emit('messageToUi',"post request received: " + req);
-  res.write('amazing!');
-  res.end();
-});
+  console.log( JSON.stringify(req.body.payload.timeStamp));
+  io.emit('messageToUi',"post request received with timestamp: " + JSON.stringify(req.body.payload.timeStamp));
 
-app.on('data',function(){
-  console.log('received!!!');
+  var timeStamp = JSON.stringify(req.body.payload.timeStamp);
+  console.log("TIMESTAMP:" + timeStamp)
+  var seconds = timeStamp.substring(18,20);
+  console.log("seconds:"+seconds);
+
+  var binarySeconds = parseInt(seconds).toString(2);
+  console.log("binary:"+binarySeconds);
+
+//fill to eight bit binary
+  for (i = binarySeconds.length; i < 8;i++){
+    binarySeconds = "0"+binarySeconds;
+  }
+  console.log("uusibinaari:"+ binarySeconds);
+
+  changeLights(binarySeconds);
+
 });
 
 //
@@ -44,12 +60,13 @@ io.on('connection', function(socket){
     });
   });
 
-  socket.on('wake service', function( uriToSend){
-    options = {
+  socket.on('wakeService', function(){
+    console.log('waking services');
+    var options = {
       method: 'post',
-      body: {"destUrl":"http://192.168.0.1:3000/"},
+      body: {"destUrl":"http://192.168.0.1:3456/"},
       json: true, // Use,If you are sending JSON data
-      url: 'http://192.168.100.113' + uriToSend,
+      url: seriveWakeURI,
       headers: {'Content-Type': 'application/json'},
 
       };
@@ -61,7 +78,32 @@ io.on('connection', function(socket){
               return;
           }
           //check zonenumber from request (because requests are async functions, they may return at any time)
-          console.log('received body: ' + JSON.stringify(body));
+          console.log('Service waken, received body: ' + JSON.stringify(body));
+          io.emit('messageToUi','Service waken, received body: ' + JSON.stringify(body));
+
+    });
+
+  });
+
+  socket.on('subscribe', function(){
+    var options = {
+      method: 'post',
+      body: {"destUrl":"http://192.168.0.1:3456/"},
+      json: true, // Use,If you are sending JSON data
+      url: SubscribeUrl,
+      headers: {'Content-Type': 'application/json'},
+
+      };
+
+      //sen request to RTU
+      request(options, function (err, res, body) {
+          if (err) {
+              console.log('Error :', err);
+              return;
+          }
+          //check zonenumber from request (because requests are async functions, they may return at any time)
+          console.log('Subscribed. received Body: ' + JSON.stringify(body));
+          io.emit('messageToUi','Subscribed. received Body: ' + JSON.stringify(body));
 
     });
 
@@ -69,49 +111,11 @@ io.on('connection', function(socket){
 
 
 //Kun käyttiksen kautta tulee wake event - käsky
-  socket.on('ChangeLights', function( sentBody){
-    changeLights(sentBody);
 
-  });
-
-  socket.on('Blink', function( sentBody){
-    var lightStatuses = "";
-    setInterval(function() {
-      lightStatuses = "";
-      for (var i = 0; i < 8; i++){
-        lightStatuses += (parseInt(Math.random()*2)).toString(); //add random number to strin 0 or 1;
-        console.log("light statuses: " + lightStatuses);
-      }
-      changeLights(lightStatuses);
-    }, 5000);
-
-
-  });
 
 
   //8 bits string in example 11110000
-  function changeLights(trueStates) {
 
-    var States = [false, false, false,false,false,false,false,false];
-    //käydään läpi truestates stringin merkit, jos 1
-    for (i = 0; i < trueStates.length; i++) {
-        if(trueStates.charAt(i) == 1){
-          States[i] = true;
-          console.log(i.toString() + "=" + "True")
-        }
-        else{
-          States[i] = false;
-          console.log(i.toString() + "=" + "False")
-
-        }
-    }
-
-    request.post(IPadressS1000+"/rest/services/changeOutput"  , {body: {"state0":States[0],"state1":States[1],"state2":States[2],"state3":States[3],"state4":States[4],"state5":States[5],"state6":States[6],"state7":States[7]}, json: true },
-    function (error, response, body) {
-      console.log('event waked at:' + IPadressS1000 +"/rest/services/changeOutput" + ' body received: '+ JSON.stringify(body));
-      io.emit('messageToUi','event waked at:' + IPadressS1000 +"/rest/services/changeOutput" +' body received: '+ JSON.stringify(body));
-    });
-  }
 
   function changeLightsSOAP(trueStates){
     var SOAP_message = '<?xml version="1.0" encoding="ISO-8859-1"?>'+
@@ -145,6 +149,30 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000,"0.0.0.0", function(){
-  console.log('listening on 3000');
+function changeLights(trueStates) {
+
+  var States = [false, false, false,false,false,false,false,false];
+  //käydään läpi truestates stringin merkit, jos 1
+  for (i = 0; i < trueStates.length; i++) {
+      if(trueStates.charAt(i) == 1){
+        States[i] = true;
+        console.log(i.toString() + "=" + "True")
+      }
+      else{
+        States[i] = false;
+        console.log(i.toString() + "=" + "False")
+
+      }
+  }
+
+  request.post(outputUri  ,
+  {body: {"state0":States[0],"state1":States[1],"state2":States[2],"state3":States[3],"state4":States[4],"state5":States[5],"state6":States[6],"state7":States[7]}, json: true },
+  function (error, response, body) {
+    console.log('outputs changed to: '+ trueStates);
+    io.emit('messageToUi','outputs changed to: '+ trueStates);
+  });
+}
+
+http.listen(3456,"0.0.0.0", function(){
+  console.log('listening on 3456');
 });
